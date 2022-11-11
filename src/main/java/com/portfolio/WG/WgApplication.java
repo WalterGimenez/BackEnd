6ced6df8794341ffbd5b04eaf7fc494bee64ef5e
WalterGimenez <wallegimenez@gmail.com>
2022-11-11 17:32:21 -0300

    cracion del proyecto backend

---
+++ src/main/java/com/portfolio/WG/WgApplication.java
@@ -0,0 +1,13 @@
+package com.portfolio.WG;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WgApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WgApplication.class, args);
+	}
+
+}
