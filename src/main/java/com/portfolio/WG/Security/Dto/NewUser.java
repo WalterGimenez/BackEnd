57ff4147d034d2639e34972fcfd06c3f59e67a27
WalterGimenez <wallegimenez@gmail.com>
2022-12-28 09:57:35 -0300

    se realizó el buid de nuevo con java 11 y Dockerfile

--- src/main/java/com/portfolio/WG/Security/Dto/NewUser.java
+++ src/main/java/com/portfolio/WG/Security/Dto/NewUser.java
@@ -6,7 +6,7 @@ import java.util.Set;
 import lombok.Getter;
 import lombok.Setter;
 
-@Getter @Setter
+
 public class NewUser {
     private String name;
     private String userName;
@@ -14,4 +14,53 @@ public class NewUser {
     private String password;
     
     private Set<String> roles = new HashSet<>();
+
+    public NewUser(String name, String userName, String email, String password) {
+        this.name = name;
+        this.userName = userName;
+        this.email = email;
+        this.password = password;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Set<String> getRoles() {
+        return roles;
+    }
+
+    public void setRoles(Set<String> roles) {
+        this.roles = roles;
+    }
+    
+    
 }
